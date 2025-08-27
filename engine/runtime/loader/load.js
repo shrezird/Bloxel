0c360431e4dd16078dbaa77324f5d29298a56e3e
shrezird <shrezird@gmail.com>
2025-08-27 00:09:47 -0500

    i demand sleep

--- engine/runtime/loader/load.js
+++ engine/runtime/loader/load.js
@@ -1,16 +1,28 @@
 (function (global) {
-  const files = [
+
+  const dataNames = [
     'error'
   ];
   
-  const scriptSrc = document.currentScript.src;
-  const basePath = scriptSrc.slice(0, scriptSrc.lastIndexOf('/') + 1);
+  const baseDirectory = document.currentScript.src.replace(/[^/]+$/, '');
+  const preloadCache = global.__preloadCache || {};
 
-  const xhr = new XMLHttpRequest();
-
-  for (const file of files) {
-    xhr.open('GET', `${basePath}data/${file}.json`, false);
-    xhr.send(null);
-    global[file] = JSON.parse(xhr.responseText);
+  for (const name of dataNames) {
+    const storageKey = '__preload_' + name;
+    sessionStorage.removeItem(storageKey);
+    if (!preloadCache[name]) {
+      let rawJson = sessionStorage.getItem(storageKey);
+      if (!rawJson) {
+        const request = new XMLHttpRequest();
+        request.open('GET', baseDirectory + 'data/' + name + '.json', false);
+        request.send(null);
+        rawJson = request.responseText;
+        sessionStorage.setItem(storageKey, rawJson);
+      }
+      preloadCache[name] = JSON.parse(rawJson);
+    }
+    global[name] = preloadCache[name];
   }
+
+  global.__preloadCache = preloadCache;
 })(window);
