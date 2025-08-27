9d37a6ac4691473a40f0b6a6db4443a62a2160f6
shrezird <shrezird@gmail.com>
2025-08-27 00:31:03 -0500

    SLEEEEEEEEEEEEEEEEEEp

--- engine/runtime/loader/load.js
+++ engine/runtime/loader/load.js
@@ -1,26 +1,15 @@
 (function (global) {
-
   const dataNames = [
     'error'
   ];
-  
   const baseDirectory = document.currentScript.src.replace(/[^/]+$/, '');
-  const preloadCache = global.__preloadCache || {};
+  const preloadCache = {};
 
   for (const name of dataNames) {
-    const storageKey = '__preload_' + name;
-    sessionStorage.removeItem(storageKey);
-    if (!preloadCache[name]) {
-      let rawJson = sessionStorage.getItem(storageKey);
-      if (!rawJson) {
-        const request = new XMLHttpRequest();
-        request.open('GET', baseDirectory + 'data/' + name + '.json', false);
-        request.send(null);
-        rawJson = request.responseText;
-        sessionStorage.setItem(storageKey, rawJson);
-      }
-      preloadCache[name] = JSON.parse(rawJson);
-    }
+    const request = new XMLHttpRequest();
+    request.open('GET', baseDirectory + 'data/' + name + '.json', false);
+    request.send(null);
+    preloadCache[name] = JSON.parse(request.responseText);
     global[name] = preloadCache[name];
   }
 
