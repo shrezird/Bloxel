be8fc6606cc9e639d62df76ad28473b3ca0c5a43
shrezird <shrezird@gmail.com>
2025-08-29 20:14:59 -0500

    noice

--- engine/runtime/services/frontend/window.py
+++ engine/runtime/services/frontend/window.py
@@ -5,20 +5,23 @@ from runtime.services.backend.utilities import p
 from runtime.services.backend.log import get_logger
 
 def start_window():
-    main = webview.create_window(
+    window = webview.create_window(
         title="Bloxel",
         url="runtime/index.html",
         maximized=True,
         min_size=(515, 360),
         js_api=get_logger()
     )
+    
     is_fullscreen = False
 
     def fullscreen():
         nonlocal is_fullscreen
         is_fullscreen = not is_fullscreen
-        main.toggle_fullscreen()
+        window.toggle_fullscreen()
         p(f"SERVICES: window.py fullscreen = {is_fullscreen}")
+        if not is_fullscreen:
+            window.maximize()
 
     def hotkey_fullscreen():
         keyboard.add_hotkey('f11', fullscreen)
