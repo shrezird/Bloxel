a0bd2776267a503f5fb012dd8d7cb5467ef14e0a
shrezird <shrezird@gmail.com>
2025-09-04 16:19:38 -0500

    Add main and window modules with window creation functionality

---
+++ engine/main.py
@@ -0,0 +1,11 @@
+from modules.window import create_window
+
+def start_main():
+    create_window()
+
+def stop_main():
+    pass
+
+if __name__ == "__main__":
+    start_main()
+    stop_main()
---
+++ engine/modules/window.py
@@ -0,0 +1,19 @@
+import glfw
+
+def create_window():
+    glfw.init()
+    glfw.window_hint(glfw.MAXIMIZED, glfw.TRUE)
+
+    window = glfw.create_window(800, 600, "Bloxel", None, None)
+    glfw.set_window_size_limits(window, 800, 600, glfw.DONT_CARE, glfw.DONT_CARE)
+    glfw.make_context_current(window)
+
+    return window
+
+window = create_window()
+
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+    glfw.swap_buffers(window)
+
+glfw.terminate()
