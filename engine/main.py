82f91537e453f68edfc018c59a5cc940c3045f82
shrezird <shrezird@gmail.com>
2025-09-05 23:11:02 -0500

    fixed burning gpu bug and formatted a bit. slep now

--- engine/main.py
+++ engine/main.py
@@ -3,8 +3,10 @@ from modules.window import create_window
 def start_main():
     create_window()
 
+
 def stop_main():
     pass
+
 
 if __name__ == "__main__":
     start_main()
--- engine/modules/window.py
+++ engine/modules/window.py
@@ -1,13 +1,13 @@
 import glfw
 
+
 def create_window():
     glfw.init()
     glfw.window_hint(glfw.MAXIMIZED, glfw.TRUE)
-
     window = glfw.create_window(800, 600, "Bloxel", None, None)
     glfw.set_window_size_limits(window, 800, 600, glfw.DONT_CARE, glfw.DONT_CARE)
     glfw.make_context_current(window)
-
+    glfw.swap_interval(1)
     return window
 
 window = create_window()
